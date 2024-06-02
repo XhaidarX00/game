6509019cfd5c76a73c75cca88aae6598bbd5c1b9
iskandar <muhamad.siskandar.zulkarnain@gmail.com>
2024-06-02 15:57:30 +0700

    fix: tagall

--- app/src/modules/tagall_patner.py
+++ app/src/modules/tagall_patner.py
@@ -452,7 +452,7 @@ async def handler_tagall_gc(client: Client, callback_query):
     msg_tagall_ = f"{msg_tagall}\n"
     count = 0
     try:
-        while len(membersList) > 0 and chat_id not in on_tagall:
+        while len(membersList) > 0 and chat_id in on_tagall:
             if datetime.now() > end_time:
                 await client.send_message(user_id, "<b>⏰ Waktu 2 menit telah habis! Tagall dihentikan.</b>")
                 on_tagall.remove(chat_id)
