f5764261f0512fa399d7a04e8660e640ee5ce11f
iskandar <muhamad.siskandar.zulkarnain@gmail.com>
2024-06-03 21:13:22 +0700

    multi game

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,9 @@
 import asyncio
 from pyrogram import filters, Client
-# from pyrogram.types import CallbackQuery
-# import vertexai
-# from vertexai.generative_models._generative_models import HarmCategory, HarmBlockThreshold
-# from vertexai.preview.generative_models import GenerativeModel
 
 from pyromod import listen
 from app.config import *
 from app.database import udb, load_data_json
-
-# # Initialize Vertex AI with project and location
-# vertexai.init(project=project_id, location=location)
-
-# # Configuration settings for the generative model
-# generation_config = {
-#     "temperature": 0.7,
-#     "top_p": 1,
-#     "top_k": 1,
-#     "max_output_tokens": 2048,
-# }
-
-# # Safety settings to control harmful content blocking thresholds
-# safety_settings = {
-#     HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-#     HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-#     HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-#     HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-# }
-
-# # Initialize generative models
-# model = GenerativeModel("gemini-pro", generation_config=generation_config, safety_settings=safety_settings)
-# vision_model = GenerativeModel("gemini-pro-vision", generation_config=generation_config, safety_settings=safety_settings)
-
-
 
 class Bot(Client):
     def __init__(self):
@@ -52,46 +23,6 @@ class Bot(Client):
         await super().stop()
 
 bot = Bot()
-
-
-
-
-# # Define helper function to retrieve a message from a conversation and handle button clicks
-# async def send_question_and_retrieve_result(prompt, message, client, keyboard):
-#     """
-#     Sends a question to the user and retrieves their response.
-
-#     Args:
-#         prompt (str): The question to ask the user.
-#         message (pyrogram.types.Message): The message object to use for sending the message.
-#         keyboard (list): The keyboard to send with the message.
-
-#     Returns:
-#         pyrogram.types.Message or None: The user's response or None if they tapped a button.
-#     """
-#     sent_message = await message.reply(prompt, reply_markup=keyboard)
-#     response = None
-
-#     def stop_callback(client, callback_query):
-#         nonlocal response
-#         response = callback_query
-#         return True
-
-#     client.add_handler(filters.CallbackQuery("stop")(stop_callback))
-
-#     while response is None:
-#         try:
-#             response = await client.listen(message.chat.id, timeout=600)
-#         except asyncio.TimeoutError:
-#             await sent_message.delete()
-#             return None
-
-#     await sent_message.delete()
-#     if isinstance(response, CallbackQuery):
-#         await response.message.delete()
-#         return None
-#     else:
-#         return response
 
 
 
--- app/__main__.py
+++ app/__main__.py
@@ -1,17 +1,9 @@
 # __main__.py
 import asyncio
 from pyrogram import *
-# from .command import handle_chat_command, handle_image_command, handle_start_command
 from app import bot
 from app.src.core.plugins import loadPlugins
 
-# Configure Pyrogram client
-# client = Client(config.session_name_bot, api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
-
-# Register command handlers
-# client.add_handler(handle_chat_command)
-# client.add_handler(handle_image_command)
-# client.add_handler(handle_start_command)
 
 loop = asyncio.get_event_loop_policy()
 event_loop = loop.get_event_loop()
